d7e1a40a2c236c4e2c8162139d17d60a32b3feec
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-02-12 18:40:26 +0700

    WIP hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/home/Hero";
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <div className="p-8 ontainer">
+      <div className="p-8 container">
         <img src="/logo/galaxy.png" className="w-32 h-11" />
       </div>
 
