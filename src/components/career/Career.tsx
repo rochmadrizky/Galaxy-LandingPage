950895791d8dbae0731daf40254a9dcdd6223721
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-02-19 13:15:30 +0700

    menambahkan carousel

--- src/components/career/Career.tsx
+++ src/components/career/Career.tsx
@@ -1,13 +1,81 @@
-import { IconDots, IconOvalVerticalFilled } from "@tabler/icons-react";
-import React from "react";
+"use client";
 
-const Career = () => {
-  const gambar = [
-    "/career/01.jpeg",
-    "/career/02.jpeg",
-    "/career/03.jpeg",
-    "/career/04.jpeg",
+import { IconPlayerTrackNext, IconPlayerTrackPrev } from "@tabler/icons-react";
+import { useState, useRef } from "react";
+
+const IsiContent = () => {
+  const konten = [
+    {
+      gambar: "/career/01.jpeg",
+    },
+    {
+      gambar: "/career/02.jpeg",
+    },
+    {
+      gambar: "/career/03.jpeg",
+    },
+
+    {
+      gambar: "/career/04.jpeg",
+    },
   ];
+
+  const [indeksSaatIni, mengaturIndeksSaatIni] = useState(0);
+  const [seretDariX, mengaturSeretDariX] = useState(0);
+  const [titikAktif, mengaturTitikAktif] = useState(0);
+  const penggeser = useRef<HTMLDivElement>(null);
+
+  const sebelumnya = () => {
+    const pengechekan =
+      indeksSaatIni === 0 ? konten.length - 1 : indeksSaatIni - 1;
+    mengaturIndeksSaatIni(pengechekan);
+    mengaturTitikAktif(pengechekan);
+  };
+
+  const selanjutnya = () => {
+    const pengechekan =
+      indeksSaatIni === konten.length - 1 ? 0 : indeksSaatIni + 1;
+    mengaturIndeksSaatIni(pengechekan);
+    mengaturTitikAktif(pengechekan);
+  };
+
+  const tekanMouse = (klik: React.MouseEvent<HTMLDivElement>) => {
+    mengaturSeretDariX(klik.pageX);
+  };
+
+  const pergerakanMouse = (geser: React.MouseEvent<HTMLDivElement>) => {
+    if (seretDariX === 0 || !penggeser.current) return;
+
+    const posisiX = geser.pageX;
+    const perbedaan = posisiX - seretDariX;
+
+    if (perbedaan > 50 && indeksSaatIni !== konten.length - 1) {
+      selanjutnya();
+      mengaturSeretDariX(0);
+    } else if (perbedaan < -50 && indeksSaatIni !== 0) {
+      sebelumnya();
+      mengaturSeretDariX(0);
+    }
+  };
+
+  const lepasKlikMouse = () => {
+    mengaturSeretDariX(0);
+  };
+
+  const titik = konten.map((_, urutan) => (
+    <span
+      key={urutan}
+      className={`h-3 w-3 bg-black ring-white ring-1 rounded-full mx-1 cursor-pointer ${
+        urutan === titikAktif && "bg-white"
+      }`}
+      onClick={() => ubahKontenDenganTitik(urutan)}
+    />
+  ));
+
+  const ubahKontenDenganTitik = (urutanIndex: number) => {
+    mengaturTitikAktif(urutanIndex);
+    mengaturIndeksSaatIni(urutanIndex);
+  };
 
   return (
     <div className="max-w-7xl mx-auto px-4">
@@ -24,27 +92,48 @@ const Career = () => {
 
           <p>voluptatem accusantium doloremque laudantium</p>
         </div>
+      </div>
 
-        <div className="pt-16 pb-24">
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
-            {gambar.map((foto, urutan) => (
-              <img
-                key={urutan}
-                src={foto}
-                className="w-[280px] h[280px] rounded-2xl"
-                alt={`Career ${urutan + 1}`}
-              />
-            ))}
-          </div>
+      <div
+        className="w-full m-auto px-4 flex flex-col items-center justify-center"
+        ref={penggeser}
+        onMouseDown={tekanMouse}
+        onMouseMove={pergerakanMouse}
+        onMouseUp={lepasKlikMouse}
+      >
+        <div className="flex items-center justify-center gap-2 py-5">
+          <button
+            onClick={sebelumnya}
+            className={`${
+              indeksSaatIni === 0
+                ? "pointer-events-none"
+                : "pointer-events-auto"
+            } bg-black rounded-full p-3`}
+          >
+            <IconPlayerTrackPrev className="text-white" />
+          </button>
 
-          <div className="flex items-center justify-center pt-16">
-            <IconDots className="h-14 w-14 text-[#C4C4C4]" />
-            <IconOvalVerticalFilled className="text-[#980000]" />
-          </div>
+          <div
+            className="w-[280px] h-[280px] rounded-3xl overflow-hidden bg-center bg-cover duration-500"
+            style={{ backgroundImage: `url(${konten[indeksSaatIni].gambar})` }}
+          ></div>
+
+          <button
+            onClick={selanjutnya}
+            className={`${
+              indeksSaatIni === konten.length - 1
+                ? "pointer-events-none"
+                : "pointer-events-auto"
+            } bg-black rounded-full p-3`}
+          >
+            <IconPlayerTrackNext className="text-white" />
+          </button>
         </div>
+
+        <div className="flex items-center justify-center pb-24">{titik}</div>
       </div>
     </div>
   );
 };
 
-export default Career;
+export default IsiContent;
